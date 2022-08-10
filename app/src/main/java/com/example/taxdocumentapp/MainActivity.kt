ac9e3841f41dce466c5342161fd0ad96847a1825
SilentSneaker <98756551+SilentSneaker@users.noreply.github.com>
2022-08-10 13:02:51 -0400

    update8/9
    First commit

--- app/src/main/java/com/example/taxdocumentapp/MainActivity.kt
+++ app/src/main/java/com/example/taxdocumentapp/MainActivity.kt
@@ -5,73 +5,88 @@ import android.content.Intent
 import android.content.pm.PackageManager
 import android.graphics.Bitmap
 import android.media.Image
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Environment
+import android.provider.ContactsContract
 import android.provider.MediaStore
+import android.view.View
 import android.widget.ImageButton
+import android.widget.ImageView
 import android.widget.Toast
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import androidx.core.content.FileProvider
+import java.io.File
+import java.io.IOException
+import java.net.URI
+import java.text.SimpleDateFormat
+import java.util.*
+import kotlin.jvm.Throws
 
 class MainActivity : AppCompatActivity() {
 //object to store values
      companion object{
          private const val CAMERA_PERMISSION_CODE = 1
-         private const val CAMERA_Request = 2
+        private const val Camera_Request_code = 2
      }
 
+    // creating file name
+    var CurrentImageFile: String? = null
 
-     override fun onCreate(savedInstanceState: Bundle?) {
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-//image button commands
-         findViewById<ImageButton>(R.id.TakePict).setOnClickListener{
-             if(ContextCompat.checkSelfPermission(this,
-                     android.Manifest.permission.CAMERA) ==
-                 PackageManager.PERMISSION_GRANTED) {
-                 val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-                 startActivityForResult(intent, CAMERA_Request)
-             } else {
-                 ActivityCompat.requestPermissions(this,
-                     arrayOf(android.Manifest.permission.CAMERA), CAMERA_PERMISSION_CODE)
-             }
-         }
-
-
     }
-// Get permissions for camera if the permissions haven't already been granted
-     override fun onRequestPermissionsResult(
-         requestCode: Int,
-         permissions: Array<out String>,
-         grantResults: IntArray
-     ) {
-         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-         if(requestCode == CAMERA_PERMISSION_CODE)
-         {
-             if(grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)
-             {
-                 val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-                 startActivityForResult(intent, CAMERA_PERMISSION_CODE)
-             }
-             else
-             {
-                 Toast.makeText(this@MainActivity,
-                     "Permission for camera was denied.", Toast.LENGTH_LONG).show()
-             }
-         }
-     }
-// if activity happens store image thumbnail
-     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-         super.onActivityResult(requestCode, resultCode, data)
-         if(resultCode == Activity.RESULT_OK)
-         {
-             if(requestCode == CAMERA_Request)
-             {
-                 val thumbNail: Bitmap = data!!.extras!!.get("data") as Bitmap
-                 findViewById<androidx.appcompat.widget.AppCompatImageView>(R.id.Thumbnail).setImageBitmap(thumbNail)
-             }
-         }
-     }
+
+    // creating unique image name
+    private fun getImageFile(): File {
+        var timeStamp: String = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
+        val imageName: String = "jpg_" +timeStamp+"_"
+        var storageDir: File? = getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+
+        var imageFile: File = File.createTempFile(imageName, ".jpeg", storageDir)
+        CurrentImageFile = imageFile.absolutePath
+        return imageFile
+    }
+
+    fun SnapPicture(view: View) {
+        val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+
+        if(cameraIntent.resolveActivity(this.packageManager) != null)
+        {
+            var imageFile: File? = null
+
+            if(imageFile == null)
+            {
+                imageFile = getImageFile()
+            }
+
+            if (imageFile != null)
+            {
+                var imageUri = FileProvider.getUriForFile(this,
+                    "com.example.android.fileprovider",imageFile)
+                cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT,imageUri)
+                startActivityForResult(cameraIntent, Camera_Request_code)
+            }
+            else
+            {
+                Toast.makeText(this, "Error", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == Camera_Request_code && requestCode == Activity.RESULT_OK)
+        {
+            thumbNail()
+        }
+    }
+    fun thumbNail() {
+        var intent: Intent? = Intent(this, findViewById(R.id.disImageView))
+        intent?.putExtra("image_path", getImageFile() )
+        startActivity(intent)
+    }
 }
-
-

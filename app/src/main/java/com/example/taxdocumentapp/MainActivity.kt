f31ab4d3484392d46335577413f5313deb314790
SilentSneaker <98756551+SilentSneaker@users.noreply.github.com>
2022-09-25 23:29:49 -0400

    Final Project Turn in

--- app/src/main/java/com/example/taxdocumentapp/MainActivity.kt
+++ app/src/main/java/com/example/taxdocumentapp/MainActivity.kt
@@ -1,96 +1,53 @@
 package com.example.taxdocumentapp
 
+import android.app.Activity
 import android.content.Intent
-
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.media.Image
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Environment
 import android.provider.MediaStore
 import android.view.View
+import android.widget.ImageButton
+import android.widget.ImageView
 import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.core.content.FileProvider
-import java.io.*
-import java.lang.Exception
-
-import java.net.URL
+import java.io.File
+import java.io.IOException
+import java.net.URI
 import java.text.SimpleDateFormat
 import java.util.*
-
+import kotlin.jvm.Throws
 
 class MainActivity : AppCompatActivity() {
-
-
+//object to store values
+     companion object{
+         private const val CAMERA_PERMISSION_CODE = 1
+     }
 
     // creating file name
     var CurrentImageFile: String? = null
-    companion object{
-        lateinit var imFile: URL
-        var read = false
-
-    }
-
-
-    lateinit var tags: List<String>
-    //object to store values
-
-        private val cameraPermissionCode = 1
-
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        if (!read) {
-
-
-            readSavedDocument()
-            read = true
-        }
+//image button commands
 
     }
 
+// Get permissions for camera if the permissions haven't already been granted
 
-    fun readSavedDocument(){
-
-        var fileInputStream: FileInputStream? = null
-        try {
-            fileInputStream = openFileInput("SavedDocuments.txt")
-
-        var inputStreamReader = InputStreamReader(fileInputStream)
-        val bufferedReader = BufferedReader(inputStreamReader)
-        val stringBuilder = StringBuilder()
-        var text: String? = null
-        while (run {
-                text = bufferedReader.readLine()
-                text
-            } != null){
-            stringBuilder.append(text)
-        }
-        var m = stringBuilder.split("╠╬").toList()
-        var documents = m[0].split("╠").toList()
-        for (file in documents){
-            var doc = file.split("*").toList()
-
-                var newDocument = Document(doc[0], doc[1],doc[2], doc[3])
-            Gallery.docList.add(newDocument)
-
-        }
-       SetDocInfo.list = m[1].split("╬").toMutableList()
-        }
-        catch (e: Exception){
-            e.printStackTrace()
-        }
-
-    }
-
-
-
+// if activity happens store image thumbnail
 
     // creating unique image name
-    private fun setImageFile(): File {
+    private fun getImageFile(): File {
         var timeStamp: String = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
-        val imageName: String = "jpg_" +timeStamp+"_"
+        var imageName: String = "jpg_" +timeStamp+"_"
         var storageDir: File? = getExternalFilesDir(Environment.DIRECTORY_PICTURES)
 
         var imageFile: File = File.createTempFile(imageName, ".jpeg", storageDir)
@@ -98,58 +55,32 @@ class MainActivity : AppCompatActivity() {
         return imageFile
     }
 
-   fun snapPicture(view: View) {
-        val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+    fun SnapPicture(view: View) {
+        var cameraIntent: Intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
 
+        if(cameraIntent.resolveActivity(packageManager)!=null)
+        {
             var imageFile: File? = null
 
             try {
-                imageFile = setImageFile()
+                imageFile = getImageFile()
             }
             catch (e: IOException)
             {
                 e.printStackTrace()
             }
-            if (imageFile != null)
+            if (imageFile!=null)
             {
-                var imageUri = FileProvider.getUriForFile(this,
-                    "com.example.taxdocumentapp",imageFile)
+                var imageUri: Uri? = FileProvider.getUriForFile(this,
+                    "com.example.android.fileprovider",imageFile)
                 cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT,imageUri)
-                startActivityForResult(cameraIntent, cameraPermissionCode)
-                imFile = imageFile.toURL()
+                startActivityForResult(cameraIntent, CAMERA_PERMISSION_CODE)
             }
-            else
-            {
-                Toast.makeText(this, "Error", Toast.LENGTH_SHORT).show()
-            }
-    }
-
-    @Override
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-
-        if (requestCode == cameraPermissionCode && resultCode == RESULT_OK){
-
-            var intent = Intent(this, SetDocInfo::class.java)
-            startActivity(intent)
         }
     }
-
-
-
-    fun goToGallery(view: View) {
-        var intent = Intent(this, Gallery::class.java)
-        startActivity(intent)
-    }
-
-    fun goToContactUs(view: View) {
-        var int = Intent(this, contactUsScreen::class.java)
-        startActivity(int)
+    fun thumbNail(view: View) {
 
     }
-    fun goToTotalsScreen(view: View) {
-        var intent = Intent(this, TotalsScreen::class.java)
-        startActivity(intent)
-    }
-
 }
+
+
